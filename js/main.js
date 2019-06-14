d9932e167e4e08959f728b10a336e5ad874751f0
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2019-06-14 01:13:04 -0400

    Reorder jquery file

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 # Don't delete this line. This main JS file needs this front matter
 ---
 
-{% include_relative bootstrap.min.js %}
 {% include_relative jquery-3.3.1.slim.min.js %}
+{% include_relative bootstrap.min.js %}
 {% include_relative google-analytics.js %}

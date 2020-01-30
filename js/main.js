bebc5568c4c4055c6e48d7d2f9bbd2997f9be6e4
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2020-01-29 23:57:04 -0800

    Add lazy loading of portfolio images

--- js/main.js
+++ js/main.js
@@ -4,4 +4,7 @@
 
 {% include_relative jquery-3.3.1.slim.min.js %}
 {% include_relative bootstrap.min.js %}
+{% include_relative lazyload.min.js %}
 {% include_relative google-analytics.js %}
+
+lazyload();
